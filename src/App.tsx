908de07d016d52daa8156399a825af91f1c874e9
SAEED <developer.saeedzhiany@gmail.com>
2019-06-04 13:54:54 +0430

    added codes to reproduce problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HighchartsReact from "highcharts-react-official";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact
+        key={"gauge chart by HC"}
+        options={{
+          title: {
+            text: "Solar Employment Growth by Sector, 2010-2016"
+          },
+
+          subtitle: {
+            text: "Source: thesolarfoundation.com"
+          },
+
+          yAxis: {
+            title: {
+              text: "Number of Employees"
+            }
+          },
+          legend: {
+            layout: "vertical" as "vertical",
+            align: "right" as "right",
+            verticalAlign: "middle" as "middle",
+          },
+
+          plotOptions: {
+            series: {
+              label: {
+                connectorAllowed: false
+              },
+              pointStart: 2010
+            }
+          },
+
+          series: [
+            {
+              type: 'line',
+              name: "Installation",
+              data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+            },
+            {
+              type: 'line',
+              name: "Manufacturing",
+              data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+            },
+            {
+              type: 'line',
+              name: "Sales & Distribution",
+              data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+            },
+            {
+              type: 'line',
+              name: "Project Development",
+              data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+            },
+            {
+              type: 'line',
+              name: "Other",
+              data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+            }
+          ],
+
+          responsive: {
+            rules: [
+              {
+                condition: {
+                  maxWidth: 500
+                },
+                chartOptions: {
+                  legend: {
+                    layout: "horizontal" as "horizontal",
+                    align: "center" as "center",
+                    verticalAlign: "bottom" as "bottom",
+                  }
+                }
+              }
+            ]
+          }
+        }}
+      />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Highcharts from 'highcharts';
+import solidgauge from 'highcharts/modules/solid-gauge';
+import exporting from 'highcharts/modules/exporting';
+import offlineExporting from 'highcharts/modules/offline-exporting';
+import exportData from "highcharts/modules/export-data";
+
+// @ts-ignore
+window.canvg = require('highcharts/lib/canvg');
+
+// @ts-ignore
+window.jspdf = require('highcharts/lib/jspdf');
+
+// @ts-ignore
+window.svg2pdf = require('highcharts/lib/svg2pdf');
+
+solidgauge(Highcharts);
+exporting(Highcharts);
+offlineExporting(Highcharts);
+exportData(Highcharts);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
